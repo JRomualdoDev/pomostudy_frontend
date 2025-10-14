6191abd3ba868701b3f8fe0f737355843b3f213d
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-14 15:12:11 -0300

    refactor(app.ts): clean file and adjust

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,34 +1,12 @@
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { ChangeDetectionStrategy, Component, ViewEncapsulation, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
- 
-import { ZardButtonComponent } from './shared/components/button/button.component';
-import { ZardInputDirective } from './shared/components/input/input.directive';
-import { ZardFormModule } from './shared/components/form/form.module';
-import { ZardDividerComponent } from "@shared/components/divider/divider.component";
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
-  templateUrl: 'app.html',
-  styleUrl: 'app.css',
-  imports: [ReactiveFormsModule, ZardButtonComponent, ZardInputDirective, ZardFormModule, ZardDividerComponent],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
+  template: `<router-outlet></router-outlet>`,
+  styleUrls: ['./app.css'],
+  imports: [RouterOutlet],
 })
 export class App {
   protected readonly title = signal('PomoStudy');
-
-  profileForm = new FormGroup({
-    username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-  });
- 
-  onSubmit() {
-    if (this.profileForm.valid) {
-      console.log('Form submitted:', this.profileForm.value);
-    }
-  }
 }
