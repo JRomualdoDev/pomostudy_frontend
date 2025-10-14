1819944f7d6afeddb860ff026d36c2617fcce476
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-14 08:47:40 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LayoutDemoFullComponent } from './index';
+import { ZardDemoTableSimpleComponent } from './task/task';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: "index", 
+        component: LayoutDemoFullComponent,
+        title: "PomoStudy - Home",
+        children: [
+            {
+                path: "task",
+                component: ZardDemoTableSimpleComponent,
+                title: "PomoStudy - Tasks"
+            }
+        ]
+    },
+    
+];
