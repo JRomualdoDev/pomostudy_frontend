ba722c0f20e7bf186841f3ce0ef806dce5ac236b
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-14 15:08:37 -0300

    refactor(main.ts): adjust http client

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    provideHttpClient(),
+    ...(appConfig.providers ?? []),
+  
+  ]
+})
   .catch((err) => console.error(err));
