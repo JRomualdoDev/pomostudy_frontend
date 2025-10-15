dd53d03b8367e3ea385bae7c24e1ae603247639a
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-15 08:49:27 -0300

    refactor(app.routes): add index dashboard, category, goals link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { ZardDemoGoalTableComponent } from './goal/goal.component';
+import { IndexTaskFormComponent } from './index/index.component';
 
 export const routes: Routes = [
 	{
@@ -16,14 +18,32 @@ export const routes: Routes = [
 		path: 'dashboard',
 		loadComponent: () =>
 			import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
-		title: 'PomoStudy - Dashboard',
-        children: [
-            {
-                path: 'task',
-                loadComponent: () => import('./task/task.component').then(m => m.ZardDemoTableSimpleComponent),
-                title: 'PomoStudy - Tasks'
-            }
-        ]
+			title: 'PomoStudy - Dashboard',
+			children: [
+				{
+					path: 'index',
+					loadComponent: () => import('./index/index.component').then(m => m.IndexTaskFormComponent),
+					title: 'PomoStudy - Dashboard'
+				},
+				{
+					path: 'task',
+					loadComponent: () => import('./task/task.component').then(m => m.ZardDemoTableSimpleComponent),
+					title: 'PomoStudy - Tasks'
+				},
+				{
+					path: 'category',
+					loadComponent: () => import('./category/category.component').then(m => m.ZardDemoCategoryTableComponent),
+					title: 'PomoStudy - Categories'
+				},
+				{
+					path: 'goals',
+					component: ZardDemoGoalTableComponent
+				},
+			]
+	},
+	{
+		path: 'index',
+		component: IndexTaskFormComponent
 	},
 	{
 		path: '**',
