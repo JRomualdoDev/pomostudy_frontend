725ef96621f9d6e730b7993ddb12cd9505e6e9a6
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-15 08:55:09 -0300

    refactor(dashboard.component): remove import not used and adjust active menu background

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { ZardButtonComponent } from '../shared/components/button/button.component';
-import { ZardSkeletonComponent } from '../shared/components/skeleton/skeleton.component';
 import { ContentComponent } from '../shared/components/layout/content.component';
 import { FooterComponent } from '../shared/components/layout/footer.component';
 import { HeaderComponent } from '../shared/components/layout/header.component';
 import { LayoutComponent } from '../shared/components/layout/layout.component';
 import { SidebarComponent, SidebarGroupComponent, SidebarGroupLabelComponent } from '../shared/components/layout/sidebar.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
 	selector: 'app-dashboard',
@@ -23,8 +24,6 @@ import { RouterLink, RouterOutlet } from '@angular/router';
         SidebarGroupComponent,
         SidebarGroupLabelComponent,
         ZardButtonComponent,
-        ZardSkeletonComponent,
-        RouterLink,
         RouterOutlet
     ],
 	templateUrl: './dashboard.html',
@@ -32,4 +31,30 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class DashboardComponent {
     year = new Date().getFullYear();
+    selectedMenu = 'dashboard';
+
+    constructor(
+        private router: Router,
+        private authService: AuthService
+    ) {
+       
+        const url = this.router.url;
+        if (url.includes('/dashboard/task')) this.selectedMenu = 'task';
+        else if (url.includes('/dashboard/category')) this.selectedMenu = 'category';
+        else if (url.includes('/dashboard/goals')) this.selectedMenu = 'goals';
+        else this.selectedMenu = 'dashboard';
+    }
+
+    selectMenu(menu: string) {
+        this.selectedMenu = menu;
+    }
+
+    navigate(path: string) {
+        this.router.navigate([path]);
+    }
+
+    logout() {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+    }
 }
