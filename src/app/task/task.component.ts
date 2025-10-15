312d1cb0f1eb9dd719c7e23d0309e0ffdf8b7b15
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-15 09:59:21 -0300

    refactor(task): add css , new items and adjusts.

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
  
 import { ZardTableComponent } from '../shared/components/table/table.component';
+import { ZardBadgeComponent } from '../shared/components/badge/badge.component';
 import { TaskService } from './task.service';
+import { DatePipe } from '@angular/common';
+import { ZardPaginationModule } from '@shared/components/pagination/pagination.module';
+
+import { signal } from '@angular/core';
  
 interface Task {
   id: number;
@@ -17,32 +23,69 @@ interface Task {
  
 @Component({
   selector: 'zard-demo-table-simple',
-  imports: [ZardTableComponent],
+  imports: [ZardTableComponent, ZardBadgeComponent,DatePipe, ZardPaginationModule, FormsModule],
   templateUrl: './task.html',
   styleUrls: ['./task.css']
 })
 export class ZardDemoTableSimpleComponent {
 
    listOfData: Task[] = [];
+   currentPage = signal<number>(1);
+   totalPages =  1;
 
    constructor(private taskService: TaskService) {}
 
    ngOnInit(): void {
-    this.taskService.getTasks().subscribe({
+      this.loadPage(this.currentPage());
+   }
+
+  loadPage(page: number): void {
+    this.taskService.getTasks(page).subscribe({
       next: (data) => {
         console.log('Dados recebidos do backend:', data);
-        
+
         if (Array.isArray(data.content)) {
           this.listOfData = data.content;
+
+          this.currentPage.set(data.pageNumber ?? page);
+
+          this.totalPages = Number(data.totalPages) || 1;
+          this.pages.set(Array.from({ length: this.totalPages }, (_, i) => i + 1));
         } else {
           this.listOfData = [];
+          this.totalPages = 1;
+          this.pages.set([1]);
         }
       },
       error: (err) => {
         console.error('Erro ao buscar tarefas:', err);
         this.listOfData = [];
+        this.totalPages = 1;
+        this.pages.set([1]);
       }
     });
   }
-  
+
+  pages = signal<number[]>(Array.from({ length: this.totalPages }, (_, i) => i + 1));
+ 
+  goToPage(page: number) {
+    this.currentPage.set(page);
+    this.loadPage(page); 
+  }
+ 
+  goToPrevious() {
+    if (this.currentPage() > 1) {
+      const prev = this.currentPage() - 1;
+      this.currentPage.set(prev);
+      this.loadPage(prev);
+    }
+  }
+
+  goToNext() {
+    if (this.currentPage() < this.totalPages) {
+      const next = this.currentPage() + 1;
+      this.currentPage.set(next);
+      this.loadPage(next); 
+    }
+  }
 }
--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -11,11 +11,33 @@ export class TaskService {
 
 	constructor(private http: HttpClient) {}
 
-  getTasks(): Observable<any> {
+  getTasks(page: number = 1, size: number = 10): Observable<any> {
     const token = localStorage.getItem('auth_token');
     const headers = new HttpHeaders({
       Authorization: token ? `Bearer ${token}` : ''
     });
-    return this.http.get<any>(this.apiUrl, { headers });
+    const params = new HttpParams()
+      .set('page', page)
+      .set('size', size);
+
+    return this.http.get<any>(this.apiUrl, { headers, params });
+  }
+
+  createTask(task: any): Observable<any> {
+    const token = localStorage.getItem('auth_token');
+    const headers = new HttpHeaders({
+      Authorization: token ? `Bearer ${token}` : '',
+      'Content-Type': 'application/json'
+    });
+    return this.http.post<any>(this.apiUrl, task, { headers });
+  }
+
+  getTasksByCategory(categoryId: number): Observable<any> {
+    const token = localStorage.getItem('auth_token');
+    const headers = new HttpHeaders({
+      Authorization: token ? `Bearer ${token}` : ''
+    });
+    // Ajuste a URL conforme seu backend espera (exemplo: /api/task/category/{id})
+    return this.http.get<any>(`${this.apiUrl}?categoryId=${categoryId}`, { headers });
   }
 }
